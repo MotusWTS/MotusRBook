ab18128a8ae0b983bfa53d1c34c7611e8ce93322
Steffi LaZerte <steffi@steffi.ca>
2019-08-02 22:12:46 +0000

    Add missing packages, formatting, spelling

--- RELEASE.R
+++ RELEASE.R
@@ -1,5 +1,24 @@
-# To preview a book chapter
-bookdown::preview_chapter("01-Introduction.Rmd", )
+# Instructions to Prepare a Book Release
+if(!file.exists("WORDLIST")) file.create("WORDLIST")
 
-# To render the entire book
+# 1. Check spelling
+# List dubious words (fix them in the .Rmd files if they are typos)
+spelling::spell_check_files(list.files(pattern = ".Rmd"), 
+                            lang = "en_CA",
+                            ignore = readLines("WORDLIST"))
+# Add remaining words to ignore list
+s <- spelling::spell_check_files(list.files(pattern = ".Rmd"), 
+                                 lang = "en_CA",
+                                 ignore = readLines("WORDLIST"))
+write(s$word, "WORDLIST", append = TRUE)
+
+# 2. Compile Chapters
+# To compile a single chapter
+bookdown::preview_chapter("05-DataCleaning.Rmd")
+
+# To compile the entire book
 bookdown::render_book("index.Rmd")
+
+# 3. Post
+# The compiled html version of the book is now in "./_book"
+
